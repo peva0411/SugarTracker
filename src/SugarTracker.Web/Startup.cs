7427b4b90dc09900c797b2407a1e2eca235315f5
Phil Evans <pevans@paylocity.com>
2016-07-27 20:36:33 -0400

    Added exception test

--- src/SugarTracker.Web/Startup.cs
+++ src/SugarTracker.Web/Startup.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using SugarTracker.Web.DataContext;
 using SugarTracker.Web.Entities;
 using SugarTracker.Web.Services;
@@ -40,6 +42,8 @@ namespace SugarTracker.Web
       var authToken = Configuration["Twwilio.AuthToken"];
       var fromPhone = Configuration["Twilio.FromPhone"];
 
+      if (string.IsNullOrEmpty(accountSid)) throw new Exception("Could not load app settings");
+
       services.AddMvc()
         .AddXmlSerializerFormatters()
         .AddXmlDataContractSerializerFormatters();
@@ -57,8 +61,9 @@ namespace SugarTracker.Web
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment environment)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment environment, ILoggerFactory loggerFactory)
     {
+    
       //if (environment.IsDevelopment())
       //{
         app.UseDeveloperExceptionPage();
